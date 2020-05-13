3247b1f8cf6a99b4675a8a4c1576816d56fe5d25
John Xu <haoxuanxu@uchicago.edu>
2020-05-13 15:04:12 -0500

    add command line argument

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os, sys, optparse
 import backtrader as bt
 from backtrader import Cerebro
 import pandas as pd
@@ -6,13 +7,32 @@ from Strategies.GoldenCross import GoldenCross
 from Strategies.BuyHold import BuyHold
 
 ##########################################################
+strategies_dict = {
+    "golden_cross": GoldenCross,
+    "buy_hold": BuyHold
+}
+##########################################################
 # Get Data
 ##########################################################
-data = pd.read_csv('SPY.csv')
+
+parser = optparse.OptionParser()
+parser.add_option("-s", "--strategy", dest="strategy", help="Which Strategy do you want to run")
+(options, args) = parser.parse_args()
+
+if options.strategy is not None:
+    strategy_key = options.strategy.strip()
+else:
+    strategy_key = input("Strategy to run >").strip()
+    if strategy_key == "":
+        raise ValueError("You must input an strategy")
+    elif strategy_key.strip() not in strategies_dict.keys():
+        raise ValueError("Strategy must part part of {}".format(strategies_dict.keys()))
+
+data = pd.read_csv('SPY.csv', index_col='date', parse_dates=True)
 cerebro = Cerebro()
 cerebro.broker.set_cash(100000)
 feed = bt.feeds.PandasData(dataname=data)
 cerebro.adddata(feed)
-cerebro.addstrategy(BuyHold)
+cerebro.addstrategy(strategies_dict[strategy_key])
 cerebro.run()
 cerebro.plot()
